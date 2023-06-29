880745067a49a85746b480ea43c96b716504b074
hbenyahia <hbenyahia@vermeg.com>
2023-06-29 12:57:53 +0100

    Add navigation with routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,14 +19,7 @@ export class HeroesComponent implements OnInit {
   heroes = HEROES;
 
 
-  selectedHero?: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
-  constructor(private heroService: HeroService,private messageService: MessageService) {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
   ngOnInit(): void {
